32fd0637bfdc3fac4196a36772c260dbe894ec93
prashanth <prashanthspring2@gmail.com>
2022-11-10 16:49:24 +0530

    first

--- edu/src/main/java/com/learn/edu/JenkinsIntegrationApplication.java
+++ edu/src/main/java/com/learn/edu/JenkinsIntegrationApplication.java
@@ -1,11 +1,22 @@
 package com.learn.edu;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class JenkinsIntegrationApplication {
 
+
+	public static Logger logger = LoggerFactory.getLogger(JenkinsIntegrationApplication.class);
+	@PostConstruct
+	public void innt()
+	{
+		logger.info("Hello Yuuuu");
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsIntegrationApplication.class, args);
 	}
